64468a225517d159474cdd5ccb6ae8ef31abaa2d
Miska23 <miskalinden@gmail.com>
2020-05-02 16:24:33 +0300

    Added new functions

--- Modules/Cards.js
+++ Modules/Cards.js
@@ -4,55 +4,82 @@ const backsides = Array.from(document.querySelectorAll(".card-backside"));
 const frontsides = Array.from(document.querySelectorAll(".card-frontside"));
 
 const initialCards = [
-  {colour: 'red'},
+  {colour: 'red',
+   pairIdentifier: 'pair-1'
+  },
   {english: 'Red',
    french: 'Rouge',
-   russian: 'Красный'
+   russian: 'Красный',
+   pairIdentifier: 'pair-1'
   },
-  {colour: 'blue'},
+  {colour: 'blue',
+   pairIdentifier: 'pair-2'
+  },
   {english: 'Blue',
    french: 'Bleu',
-   russian: 'Синий'
+   russian: 'Синий',
+   pairIdentifier: 'pair-2'
   },
-  {colour: 'green'},
+  {colour: 'green',
+  pairIdentifier: 'pair-3'
+  },
   {english: 'Green',
    french: 'Vert',
-   russian: 'Зелёный'
+   russian: 'Зелёный',
+   pairIdentifier: 'pair-3',
   },
-  {colour: 'black'},
+  {colour: 'black',
+  pairIdentifier: 'pair-4'
+  },
   {english: 'Black',
    french: 'Noir',
-   russian: 'Чёрный'
+   russian: 'Чёрный',
+   pairIdentifier: 'pair-4'
   },
-  {colour: 'white'},
+  {colour: 'white',
+
+  pairIdentifier: 'pair-5'},
   {english: 'White',
    french: 'Blanc',
-   russian: 'Белый'
+   russian: 'Белый',
+   pairIdentifier: 'pair-5'
   },
-  {colour: 'yellow'},
+  {colour: 'yellow',
+  pairIdentifier: 'pair-6'},
   {english: 'Yellow',
    french: 'Jaune',
-   russian: 'Жёлтый'
+   russian: 'Жёлтый',
+   pairIdentifier: 'pair-6'
   },
-  {colour: 'grey'},
+  {colour: 'grey',
+  pairIdentifier: 'pair-7'
+  },
   {english: 'Grey',
    french: 'Gris',
-   russian: 'Серый'
+   russian: 'Серый',
+   pairIdentifier: 'pair-7'
   },
-  {colour: 'orange'},
+  {colour: 'orange',
+  pairIdentifier: 'pair-8'},
   {english: 'Orange',
    french: 'Orange',
-   russian: 'Оранжевый'
+   russian: 'Оранжевый',
+   pairIdentifier: 'pair-8'
   },
-  {colour: 'pink'},
+  {colour: 'pink',
+  pairIdentifier: 'pair-9'},
   {english: 'Pink',
    french: 'Rose',
-   russian: 'Розовый'
+   russian: 'Розовый',
+   pairIdentifier: 'pair-9'
   },
-  {colour: 'purple'},
+  {colour: 'purple',
+  pairIdentifier: 'pair-10'
+  },
   {english: 'Purple',
    french: 'Violet',
-   russian: 'Пурпурный'
+   russian: 'Пурпурный',
+   pairIdentifier: 'pair-10'
   }
 ]
 
--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 import { initialCards, backsides, frontsides } from './Modules/Cards.js'
+
+let openedCards = [];
+let pairCounter;
+let timer;
+
+
 const shuffle = (array) => {
   let currentIndex = array.length,
     temporaryValue,
@@ -22,6 +28,7 @@ let selectedLanguage = 'russian';
 
 const createCards = () => {
   frontsides.forEach((frontside, index) => {
+    frontside.setAttribute('pairIdentifier', shuffledInitialCards[index].pairIdentifier);
     if (shuffledInitialCards[index].colour) {
       frontside.classList.add(`${shuffledInitialCards[index].colour}-card`);
     } else {
@@ -41,13 +48,75 @@ const createCards = () => {
 
 createCards();
 
+//* OK, poistetaan luokka show ja openedCardsin tyhjennys
+const closeOpenedCards = () => {
+  openedCards.forEach(openedCard => openedCard.classList.remove("show"));
+  openedCards = [];
+  console.log("openedCards was emptied");
+}
+
+//* checkIfOpenedCardsMatch-funktiossa, jotta siellä asetettu Timeout voidaan 
+//* pyyhkiä setUpGame-funktiossa clearTimedCloserin avulla.
+//* Estää tilanteen jossa tason vaihto sulkee uudessa pelissä nopesti avatun parentDivin (eli kortin), 
+//* joka oli ennen tason vaihtoa vielä auki vanhassa pelissä tasonapin painamisen aikana (ei paria muodostunut) 
+const closeCardsInTwoSeconds = () => {
+  console.log("from closeCards: timer activated");
+  timer = setTimeout(() => {
+    closeOpenedCards();
+  }, 2000);
+}
+
 //* OK
+const enableClickingInTwoSeconds = () => {
+  setTimeout(() => {
+    backsides.forEach(backside => {
+      if (!backside.children[0].classList.contains("show")) {
+        backside.style.pointerEvents = "auto";
+      }
+    });
+  }, 2000)
+}
+
 const showCard = function() {
   this.children[0].classList.toggle("show");
 }
 
+const addToOpenedCards = function() {
+  if (this.children[0].classList.contains("show")) {
+    openedCards.push(this.children[0]);
+  } else {
+    openedCards = [];
+  }
+  console.log('from addToOpenedCards, openedCards is now: ', openedCards);
+}
+
+const pairCheck = () => {
+  return openedCards[0].getAttribute('pairIdentifier') === openedCards[1].getAttribute('pairIdentifier')
+} 
+
+const checkIfOpenedCardsMatch = () => {
+  let openedCardsCounter = openedCards.length;
+  if (openedCardsCounter === 2 && pairCheck()) {
+    console.log('Pair!')
+    openedCards = [];
+    pairCounter++;
+  } else if (openedCardsCounter === 2){
+    console.log('Not a Pair!')
+    backsides.forEach(backside => backside.style.pointerEvents = "none");
+    closeCardsInTwoSeconds();
+    enableClickingInTwoSeconds();
+  }
+} 
+
+
 const setUpCardListeners = () => {
-  backsides.forEach(backside => backside.addEventListener("click", showCard));
+  backsides.forEach(backside => {
+    backside.addEventListener("click", showCard);
+    backside.addEventListener("click", addToOpenedCards);
+    backside.addEventListener("click", checkIfOpenedCardsMatch);
+  })
 }
 
 setUpCardListeners();
+
+
